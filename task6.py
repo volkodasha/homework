dbcef1c49176a93125ba615d848f6aa6fed0fd0e
volkodasha <65764400+volkodasha@users.noreply.github.com>
2020-07-06 01:11:14 +0300

    Delete task6.py

--- task6.py
+++
@@ -1,9 +0,0 @@
-start = int(input('Дистанция в первый день, км: '))
-end = int(input('Дистанция конечная, км: '))
-
-day=1
-while end - start > 0:
-    start = start + start*0.1
-    day = day + 1
-
-print(f'Чтобы пробежать {end} км, потребуется {day} дней')
