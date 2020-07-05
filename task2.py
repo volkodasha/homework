265b5482b2535948a9bf591907ef0c9056e80df5
volkodasha <65764400+volkodasha@users.noreply.github.com>
2020-07-06 01:11:51 +0300

    Delete task2.py

--- task2.py
+++
@@ -1,7 +0,0 @@
-time = int(input('Введите число секунд: '))
-hours = time // 3600
-min = (time % 3600) // 60
-sec = (time % 3600) % 60
-
-result = (f'Время: {hours:02}:{min:02}:{sec:02}')
-print(result)
