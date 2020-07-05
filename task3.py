f76ee838ab72ec990e641bf59f6f3d4c1d98b4ec
volkodasha <65764400+volkodasha@users.noreply.github.com>
2020-07-06 01:11:44 +0300

    Delete task3.py

--- task3.py
+++
@@ -1,9 +0,0 @@
-number = input('Введите любое число положительное число: ')
-
-number2 = number + number
-number3 = number2 + number
-number = int(number)
-number2 = int(number2)
-number3 = int(number3)
-result = number + number2 + number3
-print(result)
